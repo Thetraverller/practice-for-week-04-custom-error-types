f651ee01745f068ad817b665d6df2aedb2ad56ea
Tanzil <mtanzilislam1997@gmail.com>
2022-10-21 15:13:07 +0100

    done

--- error-types/maximum-length-exceeded-error.js
+++ error-types/maximum-length-exceeded-error.js
@@ -1,12 +1,23 @@
-const ValidationError = require('./validation-error');
-
 // Your code here
+
+class ValidationError extends Error {
+  constructor(message = null, ...params) {
+    super(...params);
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ValidationError);
+    }
+
+    this.name = 'ValidationError';
+    this.message = message || 'Invalid input';
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
 try {
-  module.exports = MaximumLengthExceededError;
+  module.exports = ValidationError;
 } catch {
   module.exports = null;
 }
--- error-types/validation-error.js
+++ error-types/validation-error.js
@@ -1,5 +1,20 @@
 // Your code here
 
+class ValidationError extends Error {
+  constructor(messsage = null, ...params){  ///all within constructor block
+    super(...params)
+
+
+  if(Error.captureStackTrace) {
+    Error.captureStackTrace(this, ValidationError)
+  }
+
+  this.name = ValidationError
+  this.messsage = messsage || `invalid input`
+
+}
+}
+
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
